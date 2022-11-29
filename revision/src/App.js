545685d8c9b404fc002131120e07b17f528ef55a
shidian00 <81557908+shidian00@users.noreply.github.com>
2022-11-29 09:15:28 +0000

    a

--- revision/src/App.js
+++ revision/src/App.js
@@ -1,10 +1,23 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function app() {
+  const expense = [
+    { id: 1, title: "test1", amount: 100, date: new Date(2022, 2, 2) },
+    { id: 2, title: "test2", amount: 200, date: new Date(2022, 2, 3) },
+    { id: 3, title: "test3", amount: 300, date: new Date(2022, 2, 5) },
+  ];
   return (
     <div>
       <h1>Hello World testing</h1>
-      <ExpenseItem/>
+      {expense.map((data, index) => {
+        return (
+          <ExpenseItem
+            title={data.title}
+            amount={data.amount}
+            date={data.date}
+          />
+        );
+      })}
     </div>
   );
 }
--- revision/src/components/ExpenseItem.js
+++ revision/src/components/ExpenseItem.js
@@ -1,7 +1,20 @@
 // import React from 'react'
+import "./ExpenseItem.css";
+export default function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-gb", { month: "long" });
+  const day = props.date.toLocaleString("en-gb", { month: "2-digit" });
+  const year = props.date.getFullYear();
 
-export default function ExpenseItem() {
   return (
-    <div>This is an expense item</div>
-  )
+    <div className="expense-item">
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
